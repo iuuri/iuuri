c444cdcf8a715d1b9f0059f5b961f605ac92345c
Iuri Souza <iuri.souzasantos@gmail.com>
2022-06-28 13:48:34 -0300

    commit inicial

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+const el = document.querySelector('.meu-nome');
+const text = "Iuri Souza";
+const interval = 80;
+
+function showText(el, text, interval) {
+    const char = text.split("").reverse();
+
+    const typer = setInterval(() => {
+
+if(!char.length){
+    return clearInterval(typer);
+}
+
+const next = char.pop();
+
+el.innerHTML += next;
+
+    } , interval);
+
+
+}
+
+showText(el, text, interval);
